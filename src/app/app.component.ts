94988d0a08aa6d0a3d3a31e7cde8f12ad3a762d5
rohit panwar <rohitpanwarr@gmail.com>
2020-12-30 16:02:55 +0530

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string;
+  paymentDate: string;
+  paymentAmount: Number;
+  height: Number;
+  miles: Number;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.paymentDate = value;
+  }
+
+  onAmountChange(value: string) {
+    this.paymentAmount = parseFloat(value);
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
 }
