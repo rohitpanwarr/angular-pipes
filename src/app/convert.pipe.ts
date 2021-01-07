a2b772d50841d57f17e44767aa05502935e194de
rohit panwar <rohitpanwarr@gmail.com>
2021-01-07 13:38:14 +0530

    Added pipe

---
+++ src/app/convert.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert'
+})
+export class ConvertPipe implements PipeTransform {
+
+  transform(value: any, targetUnits: string): unknown {
+    if (!value) {
+      return '';
+    }
+
+    switch (targetUnits) {
+      case 'km':
+        return value * 1.609;
+      case 'm':
+        return value * 1.609 * 1000;
+      default:
+        return value;
+    }
+  }
+
+}
